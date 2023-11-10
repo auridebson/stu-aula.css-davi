19ecc76e4e483740cb5db042e95257c08f3a9d45
Auridebson Lima <auridebson@vutus.com>
2023-11-10 18:57:30 -0300

    Upadate

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const btnForm = document.querySelector(".btn-form")
+const cpNome = document.querySelector("#cpNome")
+const cpEmail = document.querySelector("#cpEmail")
+const cpPassword = document.querySelector("#cpPassword")
+
 
 function info() {
-    alert("INFO:\nVocê está acionando o botão de login")
+    alert(`INFO:\nVocê está acionando o botão de login \n\n\n${cpNome.value}, \n${cpEmail.value}, \n${cpPassword.value}`)
 }
 
 btnForm.addEventListener("click", info)
